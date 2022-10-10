d8bcbfde606cef65753170fe89dc1bb95fe64c49
matthewds02 <descheppermatthew@gmail.com>
2022-10-10 16:11:13 +0200

    changes every test + test3 'randomBadge' + test4 'BadgeWithTooManyCharacters'

--- src/main/java/be/thomasmore/elevator/Elevator.java
+++ src/main/java/be/thomasmore/elevator/Elevator.java
@@ -2,9 +2,11 @@ package be.thomasmore.elevator;
 
 public class Elevator {
 
-    public String scan(String[] name, int verdieping) {
+    public String scan(String name, int verdieping) {
         if (name == null) return "";
-        if (name.length == 0) return "";
+        if (name.length() == 0) return "";
+        if (name.length() > 4) return "";
+        if (name.charAt(0) == 's') return "";
         return "0 10";
     }
 }
--- src/test/java/be/thomasmore/elevator/ElevatorTest.java
+++ src/test/java/be/thomasmore/elevator/ElevatorTest.java
@@ -15,13 +15,20 @@ public class ElevatorTest {
 
     @Test
     public void noValidBadge() {
-        String result = elevator.scan(new String[]{}, 0);
+        String result = elevator.scan("", 0);
         assertEquals("", result);
     }
 
     @Test
     public void randomBadge() {
-        String result = elevator.scan(new String[]{"405"}, 0);
+        String result = elevator.scan("405", 0);
         assertEquals("0 10", result);
+    }
+
+    @Test
+    public void BadgeWithTooManyCharacters() {
+        String result = elevator.scan("405845", 0);
+        System.out.println(result);
+        assertEquals("", result);
     }
 }
